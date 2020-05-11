1351b98c45c8edcb4a780c269293b4b8b54ce03c
itamar <itamardprf@gmail.com>
2020-05-11 17:33:44 -0300

    Big M case fixed

--- simplex/simplex.cpp
+++ simplex/simplex.cpp
@@ -7,29 +7,64 @@ using namespace std;
 typedef struct simplex_equation{
 
 	vector < vector<float> > equations; //values
-	vector<std::string> VB; //VB
+	vector<int> VB; //VB o 0 é a equação
 	vector<int> constraints; // constraints
+	vector<int> M_preprocess_erase;
 
 }simplex_equation;
 
-simplex_equation simplex_constructor(vector < vector<float> > equations,vector<std::string> VB,vector<int> constraints){
+simplex_equation simplex_constructor(vector < vector<float> > equations,vector<int> VB,vector<int> constraints){
+	
 	simplex_equation s1;
 	s1.equations = equations;
 	s1.VB = VB;
 	s1.constraints = constraints;
 
+	for (int j = 0; j < s1.equations[0].size(); j++) { 
+        if (equations[0][j] > 9000)
+        {	
+        	s1.M_preprocess_erase.push_back(j);
+        }
+    }
+
 	return s1;
 }
 
-int get_pivot_column(vector < vector <float> > equations){
+void preprocess_equations(simplex_equation* s1){
+
+	int row;
+	float multiplier;
+	for(int k = 0; k < s1->M_preprocess_erase.size(); k++){
+		//cout << s1->M_preprocess_erase[k];
+		row = -1;
+		multiplier = -1;
+
+		for(int i = 0; i < s1->equations.size(); i++){
+			if(s1->equations[i][s1->M_preprocess_erase[k]] > 0)
+				row = i; // pega a fileira que a gente tem que multiplicar pra zerar o M grande
+		}
+
+		multiplier = s1->equations[0][s1->M_preprocess_erase[k]] * (-1);
+
+		for(int j = 0; j < s1->equations[0].size(); j++){
+			s1->equations[0][j] += s1->equations[row][j] * multiplier;
+		}
+	}
+
+	//cout << "row = " << row << endl;
+}
+
+int get_pivot_column(simplex_equation s1){
 	/* Getting pivot column and checking optimality */
     int pivot_column = -1;
     float min_value = 0;
 
-    for(int j = 0; j < equations[0].size(); j++){
+    //cout << "opa" << s1.constraints[0] << endl;
+
+    for(int j = 0; j < s1.equations[0].size(); j++){
     	//printf("%lf\n",equations[0][j] );
-    	if((equations[0][j] < 0  && equations[0][j] < min_value)){ // tem que ser < 0 e o menor valor presente
-    		min_value = equations[0][j];
+    	if((s1.equations[0][j] < 0  && s1.equations[0][j] < min_value)){ // tem que ser < 0 e o menor valor presente
+    		min_value = s1.equations[0][j];
     		pivot_column = j;
     	}
     }
@@ -41,16 +76,16 @@ int get_pivot_column(vector < vector <float> > equations){
 	return pivot_column;
 }
 
-int get_pivot_row(vector < vector <float> > equations, int pivot_column){
+int get_pivot_row(simplex_equation s1, int pivot_column){
 	int pivot_row = -1;
 	float minimal_ratio = 100000;
 	float result;
 
 	// dividindo a primeira pelo valor dela
-	for(int i = 1; i < equations.size(); i++){ // pega do 1 porque o zero é a função que a gente quer maximizar
+	for(int i = 1; i < s1.equations.size(); i++){ // pega do 1 porque o zero é a função que a gente quer maximizar
 		
-		if(equations[i][pivot_column] > 0.9){ // ta iterando pelas linhas só se ligar, porque eu acho que aqui era pra ser maior que 0
-			result = equations[i][equations[0].size() - 1]/equations[i][pivot_column]; //faz o teste da razão mínima
+		if(s1.equations[i][pivot_column] > 0){ // ta iterando pelas linhas só se ligar, porque eu acho que aqui era pra ser maior que 0
+			result = s1.equations[i][s1.equations[0].size() - 1]/s1.equations[i][pivot_column]; //faz o teste da razão mínima
 			// basicament divide os valores das VB pelos valores da coluna pivot. o Menor valor
 			// razao minima vai ser usado pra dizer a linha pivot
 			if(result < minimal_ratio){
@@ -63,51 +98,55 @@ int get_pivot_row(vector < vector <float> > equations, int pivot_column){
 	return pivot_row; 
 }
 
-void refresh_equations(vector < vector <float> > &equations,int pivot_column, int pivot_row){
-	float pivot_element = equations[pivot_row][pivot_column];
+void refresh_equations(simplex_equation * s1,int pivot_column, int pivot_row){
+
+	float pivot_element = s1->equations[pivot_row][pivot_column];
 	
+	s1->VB[pivot_row] = pivot_column + 1;
+
 	//divide the pivot line by the pivot element
-	for(int j = 0; j < equations[0].size(); j++){
-		equations[pivot_row][j] /= pivot_element;
+	for(int j = 0; j < s1->equations[0].size(); j++){
+		s1->equations[pivot_row][j] /= pivot_element;
 	} // ai vai ficar o 1 direitinho
 	
 	//get the indices of non zero elements
 	int nonzero;
 	//vai zerar os outros elementos dessa coluna
-	for(int i = 0; i < equations.size(); i++){
+	for(int i = 0; i < s1->equations.size(); i++){
 
-		if(equations[i][pivot_column] != 0){
-			float multiplier = equations[i][pivot_column] * (-1);
+		if(s1->equations[i][pivot_column] != 0){
+			float multiplier = s1->equations[i][pivot_column] * (-1);
 
-			for(int j = 0; j < equations[0].size(); j++){
+			for(int j = 0; j < s1->equations[0].size(); j++){
 
 				if(i != pivot_row)
-					equations[i][j] += equations[pivot_row][j]*multiplier; //vai adicionando o multiplicador
+					s1->equations[i][j] += s1->equations[pivot_row][j]*multiplier; //vai adicionando o multiplicador
 			}
 		}
 	}
 
 }
 
-void solve_simplex(vector < vector <float> > &equations){
+void solve_simplex(simplex_equation* s1){
+
 	int pivot_column,pivot_row;
 
 	while(1){
-		pivot_column = get_pivot_column(equations);
+		pivot_column = get_pivot_column(*s1);
 
 		if(pivot_column == -1){
 			break;
 		}
 
-		pivot_row = get_pivot_row(equations,pivot_column);
-		refresh_equations(equations,pivot_column,pivot_row);
+		pivot_row = get_pivot_row(*s1,pivot_column);
+		refresh_equations(s1,pivot_column,pivot_row);
 	}
 }
 
-void show_tableau(vector < vector <float> > equations){
-	for (int i = 0; i < equations.size(); i++) { 
-        for (int j = 0; j < equations[i].size(); j++) 
-            cout << equations[i][j] << " "; 
+void show_tableau(simplex_equation s1){
+	for (int i = 0; i < s1.equations.size(); i++) { 
+        for (int j = 0; j < s1.equations[i].size(); j++) 
+            cout << s1.equations[i][j] << " "; 
         cout << endl; 
     } 
 }
@@ -139,27 +178,35 @@ int main(int argc, char* argv[])
 	vector<std::string> VB {"Z","X3","X4","X5"};
 	vector<int> constraints {0,0,0,0};*/
 
-	simplex_equation s1 = simplex_constructor({{-3, -5,  0, 0, 0,  0},
+	simplex_equation s1 = simplex_constructor({{-3, -5,  0, 0, 10000,  0},
     										  { 1,  0,  1, 0, 0,  4},
     										  { 0,  2,  0, 1, 0,  12},
                    							  { 3,  2,  0, 0, 1,  18} },
-                   							  {"Z","X3","X4","X5"},
-                   							  {0,0,0,0}
+                   							  {0,3,4,5},
+                   							  {0,0,0,0} //no primeiro refere-se a FO // 0 max // 1 min
+                   							  			//o resto é cada constraint // 0 <= // 1 = // 2 >=  
 												);
 
 /*
 	cout << equations.size() << endl; // number of rows
 	cout << equations[0].size() << endl; //number of columns
 */
-	for(int i = 0 ; i < s1.VB.size(); i++){
-		cout << s1.constraints[i] << endl;
-		cout << s1.VB[i] << endl;
-	}
-	show_tableau(s1.equations);
+	preprocess_equations(&s1);
 
-    solve_simplex(s1.equations);
+	show_tableau(s1);
+
+    solve_simplex(&s1);
 	
-    show_tableau(s1.equations);
+    show_tableau(s1);
 
+    for(int i = 0; i < s1.VB.size(); i++){
+    	if (i == 0)
+    	{
+   			cout << "Z" << " = " << s1.equations[i][s1.equations[0].size()-1]<<endl;
+       	}else{
+    		cout << "X" <<s1.VB[i]<< " = " << s1.equations[i][s1.equations[0].size()-1]<<endl;
+    
+    	}
+    }
 	return 0;
 }

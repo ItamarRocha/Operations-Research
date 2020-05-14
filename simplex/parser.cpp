100c3e2cbcee279bd98b1ae54b274a90deea260c
itamar <itamardprf@gmail.com>
2020-05-14 10:26:07 -0300

    parser v1

--- simplex/parser.cpp
+++ simplex/parser.cpp
@@ -93,6 +93,13 @@ std::vector<std::string> get_number_of_variables(std::map < std::string, std::ma
     return var;
 }
 
+void show_tableau(std::vector<std::vector<double>> tableau){
+    for (int i = 0; i < tableau.size(); i++) { 
+        for (int j = 0; j < tableau[i].size(); j++) 
+            cout << tableau[i][j] << " "; 
+        cout << endl; 
+    } 
+}
 
 std::vector< std::vector<double> > get_numerical_table(int n_artificial_var,int both_case, int n_slack_var,std::map < std::string ,std::map <std::string,double >> Variables , std::map <int,int> Constraints, std::map <std::string, double> RHS){
     std::vector<std::string> columns_name = get_number_of_variables(Variables);
@@ -100,44 +107,48 @@ std::vector< std::vector<double> > get_numerical_table(int n_artificial_var,int 
     int number_of_equations = Variables.size();
     
 
-    std::vector<std::vector<double>> tableau(number_of_equations, std::vector<double> (number_of_variables + n_slack_var + n_artificial_var + both_case + 1,0)); // +1 in reference to RHS
+    std::vector<std::vector<double>> tableau(number_of_equations, std::vector<double> (number_of_variables + n_slack_var + n_artificial_var + both_case*2 ,0)); // +1 in reference to RHS
     int i = 0;
-    int j = 0;
+    int k = 0;
+    bool reset = 1;
     for(auto & rows : Variables) { //rows
-            cout << rows.first << " contains: " << endl;
-        for(int k = 0; k < number_of_variables; k++) { // columns
+            //cout << rows.first << " contains: " << endl;
+        for(k = 0; k < number_of_variables - 1; k++) { // columns
             tableau[i][k] = Variables[rows.first][columns_name[k]];
-            //cout << inner_map_pair.second << " ";
-            cout << columns_name[k] << endl;
+            //cout << columns_name[k] << " = " << tableau[i][k] << endl;
         }
+        
+        // this tablear[0].size() - 1 is equivalent to the number of columns, the index of the last one
+        tableau[i][tableau[0].size() - 1] = Variables[rows.first][columns_name[number_of_variables - 1]]; // number_of_variables -1 is to find the RHS values
+        // puts in the last tableau column which is suposed to be the RHS
         i++;
-        std::cout << std::endl;   
-
+        //cout << tableau[0].size() - 1 << endl;
     }
-    //for(;i < tableau[0].size(); i++){
+    
     for(auto & constr : Constraints){
-        if(i == tableau[0].size() - 1)
+        /*show_tableau(tableau);
+        cout << endl;
+        cout << k << endl;
+        cout << constr.first << endl;*/
+        if(k == tableau[0].size() - 1)
             break;
         if(constr.second == 0){
             continue;
         }else if(constr.second == 1){
-            tableau[constr.first][i] = 1;    
+            tableau[constr.first][k] = 1;    
         }else if(constr.second == 2){
-            tableau[constr.first][i] = 1;
-            tableau[0][i] = -10000;
-            i++;
+            tableau[constr.first][k] = 1;
+            tableau[0][k] = -10000;
         }else if(constr.second == 3){
-            tableau[constr.first][i] = -1;
-            tableau[constr.first][i + 1] = 1;
-            tableau[0][i+1] = -10000;
+            tableau[constr.first][k] = -1;
+            tableau[constr.first][k + 1] = 1;
+            tableau[0][k+1] = -10000;
+            k++;
         }
         
 
-        i++;
+        k++;
     }
-    //for(auto)
-    
-
 
     return tableau;
 }
@@ -145,7 +156,7 @@ std::vector< std::vector<double> > get_numerical_table(int n_artificial_var,int 
 
 int main(){
 
-	ifstream f ("instances/p2.mps", ios::in);
+	ifstream f ("instances/p5.mps", ios::in);
 
     map<int,int> Constraints;
     int n_artificial_var = 0;
@@ -225,8 +236,6 @@ int main(){
             
             Variables[str_vec[j]][str_vec[0]] = std::stod(str_vec[j+1]);
             //std:: cout << std::stod(str_vec[j+1]) << std::endl;
-            //std::cout << str_vec[j] << std::endl; 
-            //cout << j << endl;
         }
     }
 
@@ -246,10 +255,8 @@ int main(){
         */
         for(int j = 1; j < str_vec.size(); j+= 2){ 
             
-            RHS[str_vec[j]] = std::stod(str_vec[j+1]);
+            Variables[str_vec[j]][str_vec[0]] = std::stod(str_vec[j+1]);
             //std:: cout << std::stod(str_vec[j+1]) << std::endl;
-            //std::cout << str_vec[j] << std::endl; 
-            //cout << j << endl;
         }        
     }
     /*print_map(RHS);
@@ -258,12 +265,7 @@ int main(){
 
     std::vector< std::vector<double> > tableau = get_numerical_table(n_artificial_var,both_case,n_slack_var,Variables ,Constraints,RHS);
     
-    
-    for (int i = 0; i < tableau.size(); i++) { 
-        for (int j = 0; j < tableau[i].size(); j++) 
-            cout << tableau[i][j] << " "; 
-        cout << endl; 
-    } 
+    show_tableau(tableau);
     // MAS DE BOA PQ OQ VC N MODIFICA ELE JA SETA COMO 0, AGORA É SÓ VER A PARADA DE MAXIMIZAR
     // MINIMIZAR E BOTAR AS PENALIDADES E VARIAVEIS DE FOLGA EITA MZRA QUE TEM COISA VIU
 	/* tem que passar pra um array agora essa mzra kkkkk */

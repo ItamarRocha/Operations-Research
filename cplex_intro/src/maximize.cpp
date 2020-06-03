f6e36c0fb43e228b13db4316aace60f7150a9cbc
itamar <itamardprf@gmail.com>
2020-06-03 00:05:22 -0300

    other equation

--- cplex_intro/src/maximize.cpp
+++ cplex_intro/src/maximize.cpp
@@ -9,14 +9,15 @@ int main(){
 	IloBoolVar x1(env);
 	IloBoolVar x2(env);
 	IloBoolVar x3(env);
+	IloBoolVar x4(env);
 	//adding constraints
 	//model.add(IloMinimize(env,x1 + x2 + x3));
 	
-	model.add(4*x1 + 5*x2 + 2*x3 <= 9); // creates an ilorange object
+	model.add(2*x1 + 3*x2 + 4*x3 + 5*x4 <= 7); // creates an ilorange object
 	
 	//IloObjective obj = IloMinimize(env, x1 + 2*x2 + 3*x3);
 	// not needed you can directly skip to adding it to the model
-	model.add(IloMaximize(env,5*x1 + 4*x2 + 3*x3));
+	model.add(IloMaximize(env,16*x1 + 19*x2 + 23*x3 + 28*x4));
 
 	IloCplex cplex(model);
 
@@ -38,6 +39,7 @@ int main(){
 	std::cout << "x1: " << cplex.getValue(x1) << std::endl;
 	std::cout << "x2: " << cplex.getValue(x2) << std::endl;
 	std::cout << "x3: " << cplex.getValue(x3) << std::endl;
+	std::cout << "x4: " << cplex.getValue(x4) << std::endl;
 
 	exit(0);
 }

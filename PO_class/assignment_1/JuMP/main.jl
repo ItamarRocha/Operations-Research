cc67d76d6bc8f9cfd931ef67d07de368178dc768
itamar <itamardprf@gmail.com>
2020-07-21 17:29:53 -0300

    time

--- PO_class/assignment_1/JuMP/main.jl
+++ PO_class/assignment_1/JuMP/main.jl
@@ -2,6 +2,7 @@ using JuMP
 using GLPK
 include("Data.jl")
 
+
 # Int8: -128 -> 127
 # Int16 : -32.768 -> 32.767
 # Int32 : -2.147.483.648 -> 2.147.483.647
@@ -17,53 +18,62 @@ end
 
 filepath = file[1]
 
-capacity = Array{Int64,2}
-d1 = Main.DataReader.Data(0,0,0,0,[],[])
+function solver(filepath)
+  capacity = Array{Int64,2}
+  d1 = Main.DataReader.Data(0,0,0,0,[],[])
 
-capacity, d1 = Main.DataReader.readData(filepath)
+  capacity, d1 = Main.DataReader.readData(filepath)
 
-costs = zeros(Int64,(d1.N_vertex, d1.N_vertex))
+  costs = zeros(Int64,(d1.N_vertex, d1.N_vertex))
 
-for i in 1:d1.N_vertex
-  if i in d1.start_nodes
-    costs[i,d1.end_node] = 1
-  end
-end
-
-mcfp = Model(GLPK.Optimizer)
-
-n = d1.N_vertex
-
-@variable(mcfp, x[1:n,1:n] >= 0) #ok
-
-# capacity  --------- taok
-for i in 1:n
-  for j in 1:n
-    cap = capacity[i,j]
-    @constraint(mcfp, x[i,j] - capacity[i,j] <= 0)
-  end
-end
-
-#sum 0
-for i in 1:n
-  if i in d1.start_nodes
-    @constraint(mcfp, sum(x[i,j] - x[j,i] for j in 1:n) == d1.max_possible_flow[i])
-  elseif i == d1.end_node
-    global max_end_flow = 0
-    for node in d1.start_nodes
-      max_end_flow += d1.max_possible_flow[node]
+  for i in 1:d1.N_vertex
+    if i in d1.start_nodes
+      costs[i,d1.end_node] = 1
     end
-    @constraint(mcfp, sum(x[i,j] - x[j,i] for j in 1:n) == max_end_flow * -1)
-  else
-    @constraint(mcfp, sum(x[i,j] - x[j,i] for j in 1:n) == 0) #ok tmb essa porra
   end
+
+  mcfp = Model(GLPK.Optimizer)
+
+  n = d1.N_vertex
+
+  @variable(mcfp, x[1:n,1:n] >= 0) #ok
+
+  # capacity  --------- taok
+  for i in 1:n
+    for j in 1:n
+      cap = capacity[i,j]
+      @constraint(mcfp, x[i,j] - capacity[i,j] <= 0)
+    end
+  end
+
+  #sum 0
+  for i in 1:n
+    if i in d1.start_nodes
+      @constraint(mcfp, sum(x[i,j] - x[j,i] for j in 1:n) == d1.max_possible_flow[i])
+    elseif i == d1.end_node
+      global max_end_flow = 0
+      for node in d1.start_nodes
+        max_end_flow += d1.max_possible_flow[node]
+      end
+      @constraint(mcfp, sum(x[i,j] - x[j,i] for j in 1:n) == max_end_flow * -1)
+    else
+      @constraint(mcfp, sum(x[i,j] - x[j,i] for j in 1:n) == 0) #ok tmb essa porra
+    end
+  end
+
+
+  @objective(mcfp, Min, sum(costs[i,j] * x[i,j] for i in 1:n, j in 1:n))
+
+  #println(mcfp)
+  function solve_mcfp(mcfp)
+    JuMP.optimize!(mcfp)
+    @show JuMP.termination_status(mcfp)
+    println("Max Flow = ", max_end_flow - objective_value(mcfp))
+
+  end
+  @time solve_mcfp(mcfp)
 end
 
-
-@objective(mcfp, Min, sum(costs[i,j] * x[i,j] for i in 1:n, j in 1:n))
-
-#println(mcfp)
-
-JuMP.optimize!(mcfp)
-@show JuMP.termination_status(mcfp)
-println("Max Flow = ", max_end_flow - objective_value(mcfp))
+solver("../material/instance1.txt")
+solver(filepath)
+solver("../material/instance1.txt")

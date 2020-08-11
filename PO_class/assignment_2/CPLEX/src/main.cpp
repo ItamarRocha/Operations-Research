c7c93d8f22dba3c1e5047cdb0c24b09bba795170
itamar <itamardprf@gmail.com>
2020-08-10 23:18:07 -0300

    wip

--- PO_class/assignment_2/CPLEX/src/main.cpp
+++ PO_class/assignment_2/CPLEX/src/main.cpp
@@ -8,18 +8,21 @@ using namespace std::chrono;
 
 int main(){
 
-    int n = 6;
+    int n = 7; // 6 + phantom
     int D = 100;
-    int M = 2000;
+    int multa = 2000;
+    int max_duration = 71 + 55 + 27 + 31 + 40 + 10;
 
-    std::vector<std::vector<int>> days = {{71, 60, 40},
+    std::vector<std::vector<int>> days = {{0, 0, 0},
+                                        {71, 60, 40},
                                         {55, 50, 44},
                                         {27, 23, 17},
                                         {31, 26, 20},
                                         {40, 35, 30},
                                         {10, 9, 8}};
 
-    std::vector<std::vector<int>> price = {{0, 12000, 24000},
+    std::vector<std::vector<int>> price = {{0, 0, 0},
+                                        {0, 12000, 24000},
                                         {0, 4000, 5000},
                                         {0, 13000, 17000},
                                         {0, 10000, 19000},
@@ -46,11 +49,10 @@ int main(){
         mode[i] = array;
     } // 6X3
 
-    IloArray < IloNumVarArray > c(env,n); // o dia de termino
+    IloNumVarArray C(env, n, 0, IloInfinity, ILOINT); // o dia de termino
 
-    IloVar alfa(env); //tempo de término ---> somatório das durações
-    IloVar beta(env); //tempo de atraso ---> bounded var(lb = 0) b >= (a - 100)
-    
+    IloIntVar alfa(env); //tempo de término ---> somatório das durações
+    IloIntVar beta(env, 0, max_duration); //tempo de atraso ---> bounded var(lb = 0) b >= (a - 100)
 
     // 
     //  FO
@@ -59,9 +61,12 @@ int main(){
 
     for(int i = 0; i < n; i++){
     	for(int j = 0; j < N_WAYS; j++){
-    		FO += mode[i][j] * days[i][j];
+    		FO += mode[i][j] * price[i][j];
     	}
     }
+    FO += beta*multa;
+
+    model.add(IloMinimize(env,FO));
 
 	//     
 	// Constraints
@@ -77,18 +82,86 @@ int main(){
         for(int j = 0; j < n; j++){
         	Constraint2 += x[j][i];
             Constraint1 += x[i][j];
-            IloRange r = (Constraint1 == 1); // só se liga a um
-            IloRange r = (Constraint1 == 1); // só recebe um
-            model.add(r);
+            IloRange r1 = (Constraint1 == 1); // só se liga a um
+            IloRange r2 = (Constraint2 == 1); // só recebe um
+            model.add(r1);
+            model.add(r2);
 
         }
    	}
 
-    // CT3 -> precedência
-    // CT4 -> C_j >= C_i + D_j - (1 - xij)*M
-    // CT5 -> só pode ter um modo
-    // CT6 -> alfa = sum D * modo
-    // CT7 -> beta >= (alfa - 100) , lb = 0
+    // CT3 -> C_j >= C_i + D_j - (1 - xij)*M
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            int duration = 0;
+            IloExpr Constraint4(env);
+            for(int k = 0; k < N_WAYS; k++){
+                Constraint4 += mode[j][k]*days[j][k];
+            }
+            Constraint4 += C[i] - C[j];
+            Constraint4 += -(1 - x[i][j])*max_duration;
+            IloRange r = (Constraint4 <= 0);
+            model.add(r);
+        }
+    }
+    // CT4 -> precedência
+    model.add(C[3] > C[1]);
+    model.add(C[3] > C[2]);
+    model.add(C[4] > C[3]);
+    model.add(C[5] > C[3]);
+    model.add(C[6] > C[4]);
+    model.add(C[6] > C[5]);
 
-    // env.end();
+    // CT5 -> só pode ter um modo
+    for(int i = 0; i < n; i++){
+        IloExpr Constraint5(env);
+        for(int k = 0; k < N_WAYS; k++){
+            Constraint5 += mode[i][k];
+        }
+        IloRange r = (Constraint5 == 1);
+        model.add(r);
+    }
+    // CT6 -> alfa = sum D * modo
+    IloExpr Constraint6(env);
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < N_WAYS; k++){
+            Constraint6 += days[i][k] * mode[i][k];    
+        }
+    }
+    model.add(alfa == Constraint6);
+    // CT7 -> beta >= (alfa - 100) , lb = 0
+    IloExpr Constraint7(env);
+    Constraint7 += alfa - 100;
+    model.add(beta >= Constraint7);
+
+
+
+    IloCplex solver(model);
+
+    try{
+        solver.solve();
+    }catch(...){
+        std::cout << "deu ruim" << std::endl;
+    }
+    // auto stop = high_resolution_clock::now();
+    // auto duration = duration_cast<microseconds>(stop - start); 
+      
+    // std::cout << "duration = " << duration.count() << std::endl; 
+
+    std::cout << "status:" << solver.getStatus() << std::endl;
+    std::cout << "Objective function:" << solver.getObjValue() << std::endl;
+    // for(int i = 0; i < d1->getNVertex(); i++){
+    //     for(int j = 0; j < d1->getNVertex(); j++){
+    //         if(solver.getValue(x[i][j]) > 0.01)
+    //             std::cout << " X[" << i + 1 << "][" << j + 1 << "] = " << solver.getValue(x[i][j]);
+    //     }
+    //     std::cout << std::endl;
+    // }
+    // o fluxo maximo vai ser o maximo possivel menos o que passou pelo arco que tem custo
+    //std::cout << "Max flow = " << max_flow - solver.getObjValue() << std::endl; //remove the number of paths that went through the penalty paths
+
+
+    solver.exportModel("model.lp");
+
+    env.end();
 }

7cdd0f963861c5fe6984963856a0856caf92636b
itamar <itamardprf@gmail.com>
2020-08-10 21:16:12 -0300

    notes

--- PO_class/assignment_2/CPLEX/src/main.cpp
+++ PO_class/assignment_2/CPLEX/src/main.cpp
@@ -26,6 +26,7 @@ int main(){
                                         {0, 5000, 7000},
                                         {0, 1000, 6000}};
 
+
     std::cout << days[5][2] << std::endl;
     IloEnv env;
     IloModel model(env);
@@ -37,34 +38,37 @@ int main(){
     for(int i = 0; i < n; i++){
         IloBoolVarArray array(env, n);
         x[i] = array;
-    }
+    } // n X n
 
     IloArray < IloBoolVarArray > mode(env, n);
     for(int i = 0; i < n; i++){
         IloBoolVarArray array(env, N_WAYS);
         mode[i] = array;
-    }
+    } // 6X3
 
-    IloArray < IloNumVarArray > c(env,n);
+    IloArray < IloNumVarArray > c(env,n); // o dia de termino
 
-    IloBoolVar isGreaterThanD(env);
+    IloVar alfa(env); //tempo de término ---> somatório das durações
+    IloVar beta(env); //tempo de atraso ---> bounded var(lb = 0) b >= (a - 100)
+    
+
     // 
     //  FO
-    // 
-    
+    //  Soma dos custos + beta*multa
     IloExpr FO(env);
 
     for(int i = 0; i < n; i++){
     	for(int j = 0; j < N_WAYS; j++){
     		FO += mode[i][j] * days[i][j];
     	}
-
     }
 
 	//     
 	// Constraints
-	// 
+	//
 
+    // CT1 -> só sai um
+    // CT2 -> só entra um
     for(int i = 0; i < n; i++){
 
         IloExpr Constraint1(env);
@@ -73,44 +77,18 @@ int main(){
         for(int j = 0; j < n; j++){
         	Constraint2 += x[j][i];
             Constraint1 += x[i][j];
-            if(j != n-1){
-            	IloRange r = (Constraint1 == 1); // só se liga a um
-            }else{
-            	IloRange r = (Constraint1 == 0); // no final nao se liga a ning
-            }
+            IloRange r = (Constraint1 == 1); // só se liga a um
             IloRange r = (Constraint1 == 1); // só recebe um
             model.add(r);
 
         }
    	}
-    // IloCplex mfp(model);
 
-    // auto start = high_resolution_clock::now(); 
-
-    // try{
-    //     mfp.solve();
-    // }catch(...){
-    //     std::cout << "deu ruim" << std::endl;
-    // }
-    // auto stop = high_resolution_clock::now();
-    // auto duration = duration_cast<microseconds>(stop - start); 
-      
-    // std::cout << "duration = " << duration.count() << std::endl; 
-
-    // std::cout << "status:" << mfp.getStatus() << std::endl;
-    // std::cout << "Objective function:" << mfp.getObjValue() << std::endl;
-    // for(int i = 0; i < d1->getNVertex(); i++){
-    //     for(int j = 0; j < d1->getNVertex(); j++){
-    //         if(mfp.getValue(x[i][j]) > 0.01)
-    //             std::cout << " X[" << i + 1 << "][" << j + 1 << "] = " << mfp.getValue(x[i][j]);
-    //     }
-    //     std::cout << std::endl;
-    // }
-    // // o fluxo maximo vai ser o maximo possivel menos o que passou pelo arco que tem custo
-    // std::cout << "Max flow = " << max_flow - mfp.getObjValue() << std::endl; //remove the number of paths that went through the penalty paths
-
-
-    // mfp.exportModel("model.lp");
+    // CT3 -> precedência
+    // CT4 -> C_j >= C_i + D_j - (1 - xij)*M
+    // CT5 -> só pode ter um modo
+    // CT6 -> alfa = sum D * modo
+    // CT7 -> beta >= (alfa - 100) , lb = 0
 
     // env.end();
 }
